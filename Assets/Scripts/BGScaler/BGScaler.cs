d5705a0e89e740cd344caa04a31dfdda481e91ba
haidangaten <haidangaten@hotmail.com>
2017-12-27 23:16:19 +0700

    First commit

---
+++ Assets/Scripts/BGScaler/BGScaler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScaler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        Vector3 tempScale = transform.localScale;
+
+        float height = sr.bounds.size.y; //lấy chiều cao y
+        float width = sr.bounds.size.x;
+
+        float worldHeight = Camera.main.orthographicSize * 2f; //10
+        float worldWidth = worldHeight * Screen.width / Screen.height;
+        tempScale.y = worldHeight / height;
+        tempScale.x = worldWidth / width;
+        transform.localScale = tempScale;
+        
+	}
+	
+	
+}
---
+++ Assets/Scripts/BirdController/BirdController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour {
+    public float bounceForce;
+    private Rigidbody2D myBody;
+    private Animator anim;
+
+    [SerializeField]
+    private AudioSource audioSource;
+    [SerializeField]
+    private AudioClip flyClip, pingClip, diedClip;
+
+    private bool isAlive;
+    private bool didFlap;
+	// Use this for initialization
+	void Awake () {
+        isAlive = true;
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        _BirdMovement();
+	}
+    void _BirdMovement()
+    {
+        if (isAlive)
+        {
+            if (didFlap)
+            {
+                didFlap = false;
+                myBody.velocity = new Vector2(myBody.velocity.x, bounceForce);
+                audioSource.PlayOneShot(flyClip);
+            }
+        }
+        if (myBody.velocity.y > 0)
+        {
+            float angel = 0;
+            angel = Mathf.Lerp(0, 90, myBody.velocity.y / 7);
+            transform.rotation = Quaternion.Euler(0, 0, angel);
+        }
+        else if (myBody.velocity.y == 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else if (myBody.velocity.y < 0)
+        {
+            float angel = 0;
+            angel = Mathf.Lerp(0, -90, -myBody.velocity.y / 7);
+            transform.rotation = Quaternion.Euler(0, 0, angel);
+        }
+    }
+    public void FlapButton()
+    {
+        didFlap = true;
+    }
+}
